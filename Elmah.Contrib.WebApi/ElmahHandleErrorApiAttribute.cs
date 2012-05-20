9aafac27bf60820721601f549b305ab509c97ecf
Richard Dingwall <rdingwall@gmail.com>
2012-05-20 20:33:41 +0100

    Initial commit of code.

---
+++ Elmah.Contrib.WebApi/ElmahHandleErrorApiAttribute.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace Elmah.Contrib.WebApi
+{
+    /// <summary>
+    /// ASP.NET Web API exception filter for logging errors to ELMAH. This
+    /// assumes an HttpContext is present (i.e., running under ASP.NET - it
+    /// will not work as a self-hosted WCF application).
+    /// </summary>
+    /// <remarks>
+    /// Original implementation from http://stackoverflow.com/questions/766610/how-to-get-elmah-to-work-with-asp-net-mvc-handleerror-attribute/779961#779961.
+    /// Ported from the Elmah.Contrib.Mvc package on NuGet.
+    /// </remarks>
+    public class ElmahHandleErrorApiAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            base.OnException(actionExecutedContext);
+
+            var e = actionExecutedContext.Exception;
+            if (RaiseErrorSignal(e)      // prefer signaling, if possible
+                || IsFiltered(actionExecutedContext))     // filtered?
+                return;
+
+            LogException(e);
+        }
+
+        private static bool RaiseErrorSignal(Exception e)
+        {
+            var context = HttpContext.Current;
+            if (context == null)
+                return false;
+            var signal = ErrorSignal.FromContext(context);
+            if (signal == null)
+                return false;
+            signal.Raise(e, context);
+            return true;
+        }
+
+        private static bool IsFiltered(HttpActionExecutedContext context)
+        {
+            var config = HttpContext.Current.GetSection("elmah/errorFilter")
+                         as ErrorFilterConfiguration;
+
+            if (config == null)
+                return false;
+
+            var testContext = new ErrorFilterModule.AssertionHelperContext(
+                                      context.Exception, HttpContext.Current);
+
+            return config.Assertion.Test(testContext);
+        }
+
+        private static void LogException(Exception e)
+        {
+            var context = HttpContext.Current;
+            ErrorLog.GetDefault(context).Log(new Error(e, context));
+        }
+    }
+}
---
+++ Elmah.Contrib.WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Elmah.Contrib.WebApi")]
+[assembly: AssemblyDescription("Elmah.Contrib.WebApi was designed to add ease-of-use to Elmah inside ASP.NET Web API projects.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Elmah.Contrib.WebApi")]
+[assembly: AssemblyCopyright("Copyright © Richard Dingwall 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88c3e343-261d-43ca-91a9-81b94382574e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
